c720b6993ec2270bc2e643312a0b305ebd3d6a21
Ziuro <miguellipreriarnold@gmail.com>
2023-10-21 11:14:50 -0300

    Média 2

---
+++ INICIANTE/JAVA/beecrowd 1006.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double c = sc.nextDouble();
+		double media = ((2 * a) + (3 * b) + (5 * c)) / 10;
+		System.out.printf("MEDIA = %.1f\n", media);
+	}
+}
