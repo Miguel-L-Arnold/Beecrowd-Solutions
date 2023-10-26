f86547a333bb62e577b316bf13d38df199b19b86
Ziuro <miguellipreriarnold@gmail.com>
2023-10-26 09:21:14 -0300

    Distância

---
+++ INICIANTE/JAVA/beecrowd 1016.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int km = sc.nextInt();
+		int minutos = km * 2;
+		System.out.println(minutos + " minutos");
+
+	}
+}
+
+	       /*
+         	* int minutos = (60 * km) / 30;
+         	* 
+         	*/
