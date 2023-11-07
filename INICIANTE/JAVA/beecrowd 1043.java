66b433ff3c01f37c2e3acec4c5142f4bed00d556
Ziuro <miguellipreriarnold@gmail.com>
2023-11-07 12:09:22 -0300

    Triângulo

--- INICIANTE/JAVA/beecrowd 1043.java
+++ INICIANTE/JAVA/beecrowd 1043.java
@@ -13,8 +13,8 @@ public class Main {
 			double perimetro = a + b + c;
 			System.out.printf("Perimetro = %.1f\n", perimetro);
 		} else {
-			double areaTriangulo = ((a + b) * c) / 2.0;
-			System.out.printf("Area = %.1f\n", areaTriangulo);
+			double area = ((a + b) * c) / 2.0;
+			System.out.printf("Area = %.1f\n", area);
 		}
 	}
 }
